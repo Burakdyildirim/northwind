2b8a8996c9c2110c7e0b5eeeafb1ea91af5aaaca
burak yıldırım <byildirim269@gmail.com>
2021-08-23 01:58:11 +0300

    Initial commit

---
+++ src/main/java/Kodlamaio/northwind/core/utilities/results/Result.java
@@ -0,0 +1,20 @@
+package Kodlamaio.northwind.core.utilities.results;
+
+import lombok.Data;
+
+@Data
+
+public class Result {
+	private boolean success;
+	private String message;
+	
+	public Result(boolean success) {
+		this.success = success;
+		
+	}
+	public Result(boolean success,String message) {
+		this(success);
+		this.message = message;
+		
+	}
+}
